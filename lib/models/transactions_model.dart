0be22c01d59f83cb05df22644677188797ddada3
yayahc <hamedcuenca5@gmail.com>
2024-03-02 22:59:28 +0000

    models updated

--- lib/models/transactions_model.dart
+++ lib/models/transactions_model.dart
@@ -1,6 +1,19 @@
 class TransactionModel {
   final String id;
   final String candidateId;
+  final DateTime? createdDate;
 
-  TransactionModel(this.id, this.candidateId);
+  TransactionModel(
+      {required this.id, required this.candidateId, this.createdDate});
+
+  factory TransactionModel.fromJson(Map<String, String> data) {
+    return TransactionModel(id: data['id']!, candidateId: data['candidateId']!);
+  }
+
+  Map<String, String> toJson(TransactionModel transactionModel) {
+    return {
+      "id": transactionModel.id,
+      "candidateId": transactionModel.candidateId
+    };
+  }
 }
